35fc47eff6046b4ef06a115aa106fa818931b55a
luosuihan <m18598771456@163.com>
2017-01-06 17:41:00 +0800

    登录接口新增cid字段，收藏功能，取消收藏功能，收藏列表

--- HDMobileApp/js/index.js
+++ HDMobileApp/js/index.js
@@ -9,7 +9,7 @@
 				}],
 				preloadLimit: 5 //预加载窗口数量限制(一旦超出,先进先出)默认不限制
 			});
-			var subpages = ['home_copy.html', 'trend.html', 'find.html', 'mine_new.html'];
+			var subpages = ['home_copy.html', 'collection.html','mine_new.html'];
 			var subpage_style = {
 				top: '0px',
 				bottom: '51px'
@@ -19,7 +19,7 @@
 			//创建子页面，首个选项卡页面显示，其它均隐藏；
 			mui.plusReady(function() {
 				var self = plus.webview.currentWebview();
-				for(var i = 0; i < 4; i++) {
+				for(var i = 0; i < 5; i++) {
 					var temp = {};
 					var sub = plus.webview.create(subpages[i], subpages[i], subpage_style);
 					if(i > 0) {
