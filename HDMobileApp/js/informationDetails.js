2a90be2cbc0b15dbc2a241b4d168b88134de496e
华地文化 <huadiwenhua@huadiwenhuadeiMac.local>
2017-01-12 16:18:39 +0800

    修复安卓跳转评论不成功

--- HDMobileApp/js/informationDetails.js
+++ HDMobileApp/js/informationDetails.js
@@ -336,9 +336,18 @@
 								popGesture: 'close',
 							},
 							extras: {
-								articleid: articleId,
+								articleid: articleId
 							},
 							createNew: true, //是否重复创建同样id的webview，默认为false:不重复创建，直接显示
+							show: {
+								autoShow: true,
+								aniShow: 'slide-in-right',
+								duration: mui.os.ios ? 200 : 100
+							},
+							waiting: {
+								autoShow: true,
+								title: '正在加载...',
+							}
 						});
 					});
 					/**
@@ -352,7 +361,7 @@
 								popGesture: 'close',
 							},
 							extras: {
-								articleid: articleId,
+								articleid: articleId
 							},
 							createNew: true, //是否重复创建同样id的webview，默认为false:不重复创建，直接显示
 						});
--- HDMobileApp/js/search.js
+++ HDMobileApp/js/search.js
@@ -14,7 +14,16 @@ $(function() {
 				extras: {
 					keyWord: keyWorld
 				},
-				createNew: true //是否重复创建同样id的webview，默认为false:不重复创建，直接显示
+				createNew: true, //是否重复创建同样id的webview，默认为false:不重复创建，直接显示
+				show: {
+								autoShow: true,
+								aniShow: 'slide-in-right',
+								duration: mui.os.ios ? 200 : 100
+							},
+							waiting: {
+								autoShow: true,
+								title: '正在加载...',
+							}
 			});
 		} else {
 			mui.toast('请输入搜索关键词...');
